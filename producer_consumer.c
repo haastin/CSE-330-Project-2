af2af8ff8f35630ca332e34c1553149d12f9d3cb
Austin Harris <austinharris@austins-mbp.3520.dhcp.asu.edu>
2023-03-15 20:18:32 -0700

    Changed linux/sempahore.h to just semaphore.h

--- producer_consumer.c
+++ producer_consumer.c
@@ -1,7 +1,7 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/sched.h>
-#include <linux/semaphore.h>
+#include <semaphore.h>
 #include <linux/kthread.h>
 #include <linux/init.h>
 #include <linux/proc_fs.h>
