c58939f6c9489500d94f3903f360a43898af75e2
Austin Harris <austinharris@Austins-MacBook-Pro.local>
2023-03-16 00:23:20 -0700

    All bug fixes after first testing with kernel module build

--- producer_consumer.c
+++ producer_consumer.c
@@ -1,7 +1,7 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/sched.h>
-#include <semaphore.h>
+#include <linux/semaphore.h>
 #include <linux/kthread.h>
 #include <linux/init.h>
 #include <linux/proc_fs.h>
@@ -10,6 +10,10 @@
 #include <linux/timer.h>
 #include <linux/slab.h>
 
+static int uid = 0;
+static int buff_size = 0;
+static int p = 0;
+static int c = 0;
 module_param(uid, int, 0);
 module_param(buff_size, int, 0);
 module_param(p, int, 0);
@@ -19,16 +23,15 @@ struct buffer
 {
 
     struct task_struct *head;
-    int capactity;
+    int capacity;
 };
 
 static struct buffer *buff;
-buff->capacity = buff_size;
 
 static int total_elapsed_nanosecs = 0;
 
 // need to keep track of each thread so you can stop each one from executing when you want to exit
-static struct task_struct *consumer_threads[c];
+static struct task_struct **consumer_threads;
 static struct task_struct *producer_thread;
 
 // init semaphores here
@@ -37,13 +40,13 @@ static struct semaphore full;
 static struct semaphore empty;
 static struct semaphore total_time_mutex;
 
-static int producer(void)
+static int producer(void *data)
 {
 
-    struct task_struct *current; // where the fetched process is stored
-    for_each_process(current)
+    struct task_struct *task = NULL; // where the fetched process is stored
+    for_each_process(task)
     {
-        while (!kthread_should_stop(producer_thread))
+        while (!kthread_should_stop())
         {
             // need to check if the process fetched is one that our user owns
 
@@ -59,17 +62,18 @@ static int producer(void)
             // put process task_struct in buffer
             //write details to kernel log, example print statement in the exit_function
 
-            up(buff_mutex); // release lock
-            up(full); // decrease full amt (by signaling its semaphore) by 1
+            up(&buff_mutex); // release lock
+            up(&full); // decrease full amt (by signaling its semaphore) by 1
         }
     }
+    return 0;
 }
 
-static int consumer(void)
+static int consumer(void *data)
 {
     while (true)
     {
-        while (!kthread_should_stop(producer_thread))
+        while (!kthread_should_stop())
         {
 
             if (down_interruptible(&full)) //waits to acquire full; checks if anything is currently in buffer
@@ -83,8 +87,8 @@ static int consumer(void)
 
             // remove an instance of task_struct from buffer
 
-            up(buff_mutex);// release buff lock
-            up(empty);// signal empty to make empty + 1 since we just consumed a process from buffer
+            up(&buff_mutex);// release buff lock
+            up(&empty);// signal empty to make empty + 1 since we just consumed a process from buffer
 
             // operate on task_struct data here
 
@@ -94,29 +98,34 @@ static int consumer(void)
             }
             // add elpased time to total_elapsed_time here
 
-            up(total_time_mutex); // release it
+            up(&total_time_mutex); // release it
         }
     }
+    return 0;
 }
 
 int init_func(void)
 {
     buff->capacity = buff_size;
     
-    sema_init(&buff_mutex, 1)
+    sema_init(&buff_mutex, 1);
     sema_init(&full, 0);
     sema_init(&empty, buff_size);
     sema_init(&total_time_mutex, 1);
 
-    for (int k = 0; k < p; k++)
+   int k = 0;
+    for (k = 0; k < p; k++)
     {
-        producer_thread = kthread_run(producer, NULL, "Producer-1");
+	  producer_thread = kthread_run(producer, NULL, "Producer-1");
     }
 
-    for (int i = 0; i < c; i++)
+    consumer_threads = kmalloc(c*sizeof(struct task_struct), GFP_KERNEL);
+    int i = 0;
+    for (i = 0; i < c; i++)
     {
-        consumer_thread[i] = kthread_run(consumer, NULL, "Consumer-%d", i);
+        consumer_threads[i] = kthread_run(consumer, NULL, "Consumer-%d", i);
     }
+    return 0;
 }
 
 void exit_func(void)
@@ -124,14 +133,16 @@ void exit_func(void)
 
     kthread_stop(producer_thread);
 
-    for (int e = 0; int e < c; e++)
+    int e = 0;
+    for (e = 0; e < c; e++)
     {
         kthread_stop(consumer_threads[e]);
         consumer_threads[e] == NULL;
     }
     // logic for implmenting nanoseconds to HH:MM:SS here, and fill in the rest below
-    printk(KERN_INFO "The total elapsed time of all processes for UID %d is %d%d:%d%d:%d%d", );
+   // printk(KERN_INFO "The total elapsed time of all processes for UID %d is %d%d:%d%d:%d%d", );
 }
 
 module_init(init_func);
 module_exit(exit_func);
+MODULE_LICENSE("GPL");
