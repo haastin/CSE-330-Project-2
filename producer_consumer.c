6a06f8fcc6a8fbd54e2325cd27b55d86b9534802
Austin Harris <austinharris@Austins-MacBook-Pro.local>
2023-03-16 23:30:29 -0700

    more syntax changes

--- producer_consumer.c
+++ producer_consumer.c
@@ -54,7 +54,7 @@ static int producer(void *data)
         while (!kthread_should_stop())
         {
             if (task->cred->uid.val == uid) // need to check if the process fetched is one that our user owns
-            { 
+            {
 
                 if (down_interruptible(&empty)) // acquire empty; checks if any open places left in buffer
                 {
@@ -64,20 +64,22 @@ static int producer(void *data)
                 {
                     break; // is only evaluated when a signal is received from down_interruptible
                 }
-                //insert at tail, take from tail
-                if(head != NULL){
-                struct buff_node *curr_tail = tail;  // put process task_struct in buffer
-                struct buff_node *new_buff_node = kmalloc(sizeof(struct buff_node), GFP_KERNEL);
-                tail->next = new_buff_node;
-                tail = new_buff_node;
-                tail->next = NULL;
-                tail->prev = curr_tail;
-                tail->index = curr_tail->index + 1;
-                tail->serial_no = tasks_so_far;
-                tail->fetched_task = task;
-                tasks_so_far++; //dont need a semaphore for this since only one will be accessing their critical section at a time
+                // insert at tail, take from tail
+                if (head != NULL)
+                {
+                    struct buff_node *curr_tail = tail; // put process task_struct in buffer
+                    struct buff_node *new_buff_node = kmalloc(sizeof(struct buff_node), GFP_KERNEL);
+                    tail->next = new_buff_node;
+                    tail = new_buff_node;
+                    tail->next = NULL;
+                    tail->prev = curr_tail;
+                    tail->index = curr_tail->index + 1;
+                    tail->serial_no = tasks_so_far;
+                    tail->fetched_task = task;
+                    tasks_so_far++; // dont need a semaphore for this since only one will be accessing their critical section at a time
                 }
-                else{ //head should already be allocated statically
+                else
+                { // head should already be allocated statically
                     head->fetched_task = task;
                     head->next = NULL;
                     head->prev = NULL;
@@ -115,24 +117,26 @@ static int consumer(void *data)
 
             struct buff_node *temp = tail; // remove an instance of task_struct from buffer
             struct buff_node *new_tail = tail->prev;
-            if(new_tail != NULL){ //shouldnt ever need this since we check this condition with a semaphore already
+            if (new_tail != NULL)
+            { // shouldnt ever need this since we check this condition with a semaphore already
                 new_tail->next = NULL;
                 tail = new_tail;
                 kfree(temp);
             }
-            else{
+            else
+            {
                 head = NULL;
                 tail = head;
                 kfree(temp);
             }
             up(&buff_mutex); // release buff lock
             up(&empty);      // signal empty to make empty + 1 since we just consumed a process from buffer
-            uint64_t nanosecs_elapsed = ktime_get_ns() - temp->fetched_task->start_time;
-            uint64_t secs_elapsed = nanosecs_elapsed*(1,000,000,000);
-            uint64_t hours_elapsed = secs_elapsed/3600;
-            uint64_t minutes_elapsed = (secs_elapsed % 3600)/60;
-            uint64_t secs_elapsed_remaining = secs_elapsed - hours_elapsed*3600 - minutes_elapsed*60;
-            printk(KERN_INFO "%s Consumed Item#-%d on buffer index:%d PID:%d Elapsed Time- %d:%d:%d", current->comm, temp->serial_no, temp->index, task_pid_nr(temp->fetched_task), hours_elapsed, minutes_elapsed, seconds_elapsed_remaining);            // operate on task_struct data here
+            nanosecs_elapsed = ktime_get_ns() - temp->fetched_task->start_time;
+            secs_elapsed = nanosecs_elapsed * (1, 000, 000, 000);
+            hours_elapsed = secs_elapsed / 3600;
+            minutes_elapsed = (secs_elapsed % 3600) / 60;
+            secs_elapsed_remaining = secs_elapsed - hours_elapsed * 3600 - minutes_elapsed * 60;
+            printk(KERN_INFO "%s Consumed Item#-%d on buffer index:%d PID:%d Elapsed Time- %d:%d:%d", current->comm, temp->serial_no, temp->index, task_pid_nr(temp->fetched_task), hours_elapsed, minutes_elapsed, secs_elapsed_remaining); // operate on task_struct data here
 
             if (down_interruptible(&total_time_mutex)) // get a lock for total_elpased_nanosecs
             {
@@ -183,10 +187,10 @@ void exit_func(void)
         kfree(consumer_threads[e]);
     }
     // logic for implmenting nanoseconds to HH:MM:SS here, and fill in the rest below
-    secs_elapsed = total_elapsed_nanosecs*(1,000,000,000);
-    hours_elapsed = secs_elapsed/3600;
-    minutes_elapsed = (secs_elapsed % 3600)/60;
-    secs_elapsed_remaining = secs_elapsed - hours_elapsed*3600 - minutes_elapsed*60;
+    uint64_t secs_elapsed = total_elapsed_nanosecs * (1, 000, 000, 000);
+    uint64_t hours_elapsed = secs_elapsed / 3600;
+    uint64_t minutes_elapsed = (secs_elapsed % 3600) / 60;
+    uint64_t secs_elapsed_remaining = secs_elapsed - hours_elapsed * 3600 - minutes_elapsed * 60;
     printk(KERN_INFO "The total elapsed time of all processes for UID %d is %d:%d:%d", uid, hours_elapsed, minutes_elapsed, secs_elapsed_remaining);
 }
 
