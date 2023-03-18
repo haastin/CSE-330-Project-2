a2a1729f24c8e0198c0510df0bede7394784d295
Austin Harris <austinharris@Austins-MacBook-Pro.local>
2023-03-18 00:52:10 -0700

    p

--- producer_consumer.c
+++ producer_consumer.c
@@ -243,10 +243,15 @@ void exit_func(void)
     }
     //printk(KERN_INFO "released consumer threads");
     // logic for implmenting nanoseconds to HH:MM:SS here, and fill in the rest below
-    unsigned long long int secs_elapsed = total_elapsed_nanosecs /(1000000000);
-    unsigned long long int hours_elapsed = secs_elapsed / 3600;
-    unsigned long long int minutes_elapsed = (secs_elapsed % 3600) / 60;
-    unsigned long long int secs_elapsed_remaining = secs_elapsed - hours_elapsed * 3600 - minutes_elapsed * 60;
+    unsigned long long int secs_elapsed = 0;
+    printk(KERN_INFO "total ns ela: %llu", total_elapsed_nanosecs);
+    secs_elapsed = total_elapsed_nanosecs /(1000000000);
+    unsigned long long int hours_elapsed = 0;
+    hours_elapsed = secs_elapsed / 3600;
+    unsigned long long int minutes_elapsed = 0;
+    minutes_elapsed = (secs_elapsed % 3600) / 60;
+    unsigned long long int secs_elapsed_remaining = 0;
+    secs_elapsed_remaining = secs_elapsed - hours_elapsed * 3600 - minutes_elapsed * 60;
     printk(KERN_INFO "The total elapsed time of all processes for uuid %d is %llu:%llu:%llu\n", uuid, hours_elapsed, minutes_elapsed, secs_elapsed_remaining);
     //kfree(producer_thread);
     //producer_thread = NULL;
